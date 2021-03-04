b477f0fe45182227d21c2384da302d40cd51268a
Tianny <tiannyxie@gmail.com>
2021-03-04 10:14:30 +0800

    add

--- The Way To Go/ch6/18/defer_closure.go
+++ The Way To Go/ch6/18/defer_closure.go
@@ -5,12 +5,12 @@ import "fmt"
 func main() {
 	for i := 0; i < 3; i++ {
 		defer func ()  {
-			fmt.Print(i)
+			fmt.Print(i) // 传递 i 的引用
 		}()
 	}
 
 	for i := 0; i < 3; i++ {
-		defer fmt.Print(i)
+		defer fmt.Print(i) // 传递 i 的拷贝
 	}
 
 }
