9ce2218e7735d52bc73be960c7b58b657e48cd42
Tianny <tiannyxie@gmail.com>
2021-03-06 16:12:47 +0800

    add

--- The Way To Go/ch10/5/structs_tag.go
+++ The Way To Go/ch10/5/structs_tag.go
@@ -11,6 +11,8 @@ type tagType struct { //tags
 	field3 int    "How much there are"
 }
 
+// 在一个变量上调用 reflect.TypeOf() 可以获取变量的正确类型
+// 如果变量是一个结构体类型，就可以通过 Field 来索引结构体的字段，然后就可以使用 Tag 属性
 func refTag(tt tagType, ix int)  {
 	ttType := reflect.TypeOf(tt)
 	ixField := ttType.Field(ix)
