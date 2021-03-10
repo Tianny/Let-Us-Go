3f6f4346e6c339b27a5bd72155057d7f78034698
Tianny <tiannyxie@gmail.com>
2021-03-11 07:00:24 +0800

    add

--- The Way To Go/ch11/1/interfaces.go
+++ The Way To Go/ch11/1/interfaces.go
@@ -23,14 +23,13 @@ func main() {
 	fmt.Printf("The square has area: %f\n", sq1.area())
 
 	var areaIntf shaper
+	areaIntf = sq1 
 	// 将一个 Square 类型的变量赋值给一个接口类型的变量
 	// 现在接口变量包含一个指向 Square 变量的引用，通过它可以调用 Square 上的方法 Area()
 	// 这就是多态的 Go 版本
-	areaIntf = sq1 
+	
 	// shorter,without separate declaration:
 	// areaIntf := Shaper(sq1)
-	// or even:
-	// areaIntf := sq1
 	fmt.Printf("The square has area: %f\n", areaIntf.area())
 
 }
