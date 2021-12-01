3cccc1a78fb8f05548dc14eeddf77f5565832044
Tianny <tiannyxie@163.com>
2021-12-01 20:33:02 +0800

    fuck slice

--- The Way To Go/11-slice/append_slice_correct.go
+++ The Way To Go/11-slice/append_slice_correct.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func modifySliceCorrect(innerSlice *[]string) {
+	*innerSlice = append(*innerSlice, "a")
 	(*innerSlice)[0] = "b"
 	(*innerSlice)[1] = "b"
 	fmt.Println(*innerSlice)
